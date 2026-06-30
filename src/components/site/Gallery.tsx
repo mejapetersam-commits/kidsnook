e8ce8fff794447f2e42baeb246f4a836eccc570d
mejapetersam-commits <mejapetersam@gmail.com>
2026-06-30 11:18:39 -0700

    Update Gallery.tsx

--- src/components/site/Gallery.tsx
+++ src/components/site/Gallery.tsx
@@ -2,7 +2,7 @@ import { Instagram } from "lucide-react";
 import { INSTAGRAM_URL } from "@/lib/site-data";
 import girl from "@/assets/girl-glasses.png.asset.json";
 import mother from "@/assets/mother-child.png.asset.json";
-import birthday from "@/assets/birthday.png.asset.json";
+import birthday from "@/assets/birthday.png";
 import gaming from "@/assets/gaming.jpg";
 import library from "@/assets/library.jpg";
 import outdoor from "@/assets/outdoor.jpg";
